808010c132c50e491e5817d42de796534d4c1222
Paul-Hess <pauljacksonhess@gmail.com>
2016-01-16 21:36:00 -0800

    fix issue, text is not copied to new row at end of table

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $('button.add-row').click(function() {
     var newRow = $('.before-row').clone().removeClass('before-row');
+    newRow.find('textarea').val('');
     $('tr').last().after(newRow);
   });
 });
