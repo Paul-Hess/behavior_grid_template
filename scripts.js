02a478fb36934401d86c7f07234ff4de91725d5d
Paul-Hess <pauljacksonhess@gmail.com>
2016-01-16 21:11:29 -0800

    fix issue, new rows now added at end of table

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('button.add-row').click(function() {
-    var newRow = $('.before-row').clone(true).removeClass('before-row');
-    $('#after-row').after(newRow);
+    var newRow = $('.before-row').clone().removeClass('before-row');
+    $('tr').last().after(newRow);
   });
 });
